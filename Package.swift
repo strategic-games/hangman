6883c16d1ef5e413496e8c9011a4ef74b64619b9
Tamara Cook <tamara.cook@icloud.com>
2018-06-20 03:07:09 +0200

    Add B-tree package

--- Package.swift
+++ Package.swift
@@ -11,6 +11,7 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
+      .package(url: "https://github.com/attaswift/BTree", from: Version("4.0.0"))
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
