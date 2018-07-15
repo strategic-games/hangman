aea708bd166f708b261dd31547d095acc78afb80
Tamara Cook <tamara.cook@icloud.com>
2018-07-15 18:33:05 +0200

    Make Direction toggleable

--- Sources/Begriffix/Direction.swift
+++ Sources/Begriffix/Direction.swift
@@ -1,8 +1,20 @@
-//
-//  Direction.swift
-//  Begriffix
-//
-//  Created by Tamara Cook on 09.07.18.
-//
-
-import Foundation
+enum Direction: CaseIterable {
+  case Horizontal, Vertical
+  func kernel(_ count: Int) -> Matrix<Int> {
+    let size: Dimensions
+    switch self {
+    case .Horizontal: size = Dimensions(1, count)
+    case .Vertical: size = Dimensions(count, 1)
+    }
+    return Matrix(repeating: 1, size: size)
+  }
+  func toggled() -> Direction {
+    switch self {
+    case .Horizontal: return .Vertical
+    case .Vertical: return .Horizontal
+    }
+  }
+  mutating func toggle() {
+    self = toggled()
+  }
+}
