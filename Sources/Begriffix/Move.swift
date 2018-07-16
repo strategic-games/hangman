b18d0cfd1c1a522c73b5e3032e9bf8346af59423
Tamara Cook <tamara.cook@icloud.com>
2018-07-16 20:08:56 +0200

    Validate moves' crossing words

--- Sources/Begriffix/Move.swift
+++ Sources/Begriffix/Move.swift
@@ -1,36 +1,9 @@
 struct Move {
-  typealias Board = Matrix<Character?>
-  enum Direction {
-    case Horizontal, Vertical
-    func kernel(_ count: Int) -> Matrix<Int> {
-      let size: Dimensions
-      switch self {
-      case .Horizontal: size = Dimensions(1, count)
-      case .Vertical: size = Dimensions(count, 1)
-      }
-      return Matrix(repeating: 1, size: size)
-    }
-  }
-  var start: Position
-  var direction: Direction
-  var count: Int {
-    didSet {word = ""}
-  }
-  var word: String {
-    didSet {count = word.count}
-  }
-  init(start: Position = Position(0, 0), direction: Direction, word: String) {
-    self.start = start
-    self.direction = direction
-    self.word = word
-    self.count = word.count
-  }
-  init(start: Position = Position(0, 0), direction: Direction, count: Int) {
-    self.start = start
-    self.direction = direction
-    self.count = count
-    self.word = ""
-  }
+  let start: Position
+  let direction: Direction
+  let word: String
+  let sum: Int
+  var count: Int {return word.count}
   func positions() -> [Position] {
     let r = 0..<count
     switch direction {
@@ -40,21 +13,16 @@ struct Move {
       return r.map {Position(start.i+$0, start.j)}
     }
   }
-  func write(on board: inout Board) {
-    for (p, c) in zip(positions(), word) {
-      board[p] = c
-    }
-  }
-  func pattern(_ board: Board) -> String {
-    if count == 0 {return ""}
-    let positions = self.positions()
-    var pattern = ""
-    for p in positions {
-      pattern.append(board[p] ?? "?")
-    }
-    return pattern
-  }
   func kernel() -> Matrix<Int> {
     return direction.kernel(self.count)
+  }
+  /// Return the range of lines that would be crossed by this move
+  func lines() -> Range<Int> {
+    let start: Int
+    switch direction {
+    case .Horizontal: start = self.start.j
+    case .Vertical: start = self.start.i
+    }
+    return start..<start+word.count
   }
 }
--- Sources/Begriffix/Player.swift
+++ Sources/Begriffix/Player.swift
@@ -2,7 +2,13 @@ import Foundation
 import HangMan
 
 class Player {
+  struct Place {
+    let start: Position
+    let direction: Direction
+    let words: [String]
+  }
   static let dict: Set<String> = loadDict("german") ?? Set()
+  static let radix: Radix = createRadix()
   static func loadDict(_ lang: String) -> Set<String>? {
     let b = Bundle(for: Radix.self)
     guard let url = b.url(forResource: "dictionaries/\(lang)", withExtension: "txt") else {return nil}
@@ -11,25 +17,62 @@ class Player {
     data.remove("")
     return data
   }
-  let vocabulary = Radix()
-  init() {
-    let words = Player.dict
-    vocabulary.insert(words)
+  static func createRadix() -> Radix {
+    let radix = Radix()
+    radix.insert(Player.dict)
+    return radix
   }
   func deal(with state: State) -> Move? {
-    let dir: Move.Direction = state.player ? .Horizontal : .Vertical
-    let lower = state.options.contains(.includeThree) ? 3 : 4
-    for count in stride(from: 8, through: lower, by: -1) {
-      var move = Move(direction: dir, count: count)
-      let p = state.scan(for: move)
-   if p.count == 0 {continue}
-      move.start = p[0]
-      let pattern = move.pattern(state.board)
-      let matches = vocabulary.match(pattern)
-      if matches.count == 0 {continue}
-      move.word = matches[0]
-      return move
+    switch state.phase {
+    case .Restricted: return dealRestricted(with: state)
+    case .Liberal: return dealLiberal(with: state)
+    case .KnockOut: return nil
     }
-    return nil
+  }
+  func dealRestricted(with state: State) -> Move? {
+    var places = [Place]()
+    let dir: Direction = state.player ? .Horizontal : .Vertical
+    for count in stride(from: 8, through: 4, by: -1) {
+      let p = state.scan(direction: dir, count: count)
+      if p.isEmpty {continue}
+      for s in p {
+        let pattern = state.board[s, dir, count].map {$0 ?? "?"}
+        let matches = Player.radix.match(String(pattern))
+        if matches.isEmpty {continue}
+        places.append(Place(start: s, direction: dir, words: matches))
+      }
+    }
+    return select(from: places)
+  }
+  func dealLiberal(with state: State) -> Move? {
+    var places = [Place]()
+    for dir in Direction.allCases {
+      for count in stride(from: 8, through: 3, by: -1) {
+        let p = state.scan(direction: dir, count: count)
+        if p.isEmpty {continue}
+        for s in p {
+          let matches = match(state: state, start: s, dir: dir, count: count)
+          if matches.isEmpty {continue}
+          places.append(Place(start: s, direction: dir, words: matches))
+        }
+      }
+    }
+    return select(from: places)
+  }
+  func match(state: State, start: Position, dir: Direction, count: Int) -> [String] {
+    let pattern = state.board[start, dir, count].map {$0 ?? "?"}
+    let matches = Player.radix.match(String(pattern)).filter {validate(state, with: Move(start: start, direction: dir, word: $0, sum: 0))}
+    return matches
+  }
+  func validate(_ state: State, with move: Move) -> Bool {
+    let newState = state + move
+    let words = newState.words(move.direction.toggled(), lines: move.lines())
+    return words.joined().allSatisfy {Player.radix.contains($0)}
+  }
+  func select(from places: [Place]) -> Move? {
+    guard let place = places.randomElement() else {return nil}
+    guard let word = place.words.randomElement() else {return nil}
+    let wordSum = places.map({$0.words.count}).sum()
+    return Move(start: place.start, direction: place.direction, word: word, sum: wordSum)
   }
   }
