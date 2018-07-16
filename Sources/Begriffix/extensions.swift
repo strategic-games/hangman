f4fa7f562263792236d463e0b30811b3e487912f
Tamara Cook <tamara.cook@icloud.com>
2018-07-16 19:02:28 +0200

    Add word method to RandomAccessCollection

--- Sources/Begriffix/extensions.swift
+++ Sources/Begriffix/extensions.swift
@@ -16,3 +16,23 @@ extension Sequence where Element == Character? {
     return self.split(separator: nil).map({w in String(w.compactMap({$0}))}).filter {$0.count > 2}
   }
 }
+
+extension RandomAccessCollection where Element == Character? {
+  func word(around i: Index) -> String? {
+    var start = i
+    var end = i
+    while end > startIndex {
+      let next = index(before: start)
+      if self[next] == nil {break}
+      start = next
+    }
+    while end < endIndex {
+      let next = index(after: end)
+      if self[next] == nil {break}
+      start = next
+    }
+    let letters = self[start..<end]
+    if letters.count <= 2 {return nil}
+    return String(letters.compactMap {$0})
+  }
+}
