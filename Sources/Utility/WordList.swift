7b6da74a19334044ebf8a9abd121b744a0376575
Tamara Cook <tamara.cook@icloud.com>
2018-09-06 07:20:44 +0200

    Make WordList CaseIterable

--- Sources/Utility/WordList.swift
+++ Sources/Utility/WordList.swift
@@ -1,13 +1,16 @@
 import Foundation
 
 /// Word lists that can be used for verbal games
-public enum WordList: String {
+public enum WordList: String, CaseIterable {
   case ScrabbleDict = "german"
   case English = "english"
   case Derewo = "derewo-v-100000t-2009-04-30-0.1"
-  public var url: URL? {
-    return URL(fileURLWithPath: "dictionaries/\(self.rawValue).txt")
-    //return Bundle(path: "hangman")?
-      //.url(forResource: self.rawValue, withExtension: "txt", subdirectory: "dictionaries")
+  var url: URL? {
+    return Bundle(for: Radix.self)
+      .url(forResource: self.rawValue, withExtension: "txt", subdirectory: "dictionaries")
+  }
+  public func load() -> String? {
+    guard let url = self.url else {return nil}
+    return try? String(contentsOf: url)
   }
 }
