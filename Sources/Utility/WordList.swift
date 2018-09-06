f4340b28dd782a14e60e049b3d8f07821a66e785
Tamara Cook <tamara.cook@icloud.com>
2018-09-06 08:48:44 +0200

    Document WordList

--- Sources/Utility/WordList.swift
+++ Sources/Utility/WordList.swift
@@ -2,13 +2,26 @@ import Foundation
 
 /// Word lists that can be used for verbal games
 public enum WordList: String, CaseIterable {
-  case ScrabbleDict = "german"
-  case English = "english"
-  case Derewo = "derewo-v-100000t-2009-04-30-0.1"
+  /// A very long german word list from ScrabbleDict
+  case ScrabbleDict
+  /// The „Wortformenliste“ by Derewo, a german corpus-based project
+  case Derewo
+  /// An english word list, unknown origin
+  case English
+  /// The name of the resource text file
+  var resource: String {
+    switch self {
+    case .ScrabbleDict: return "german"
+    case .Derewo: return "derewo-v-100000t-2009-04-30-0.1"
+    case .English: return "english"
+    }
+  }
+  /// The resource URL in the bundle
   var url: URL? {
     return Bundle(for: Radix.self)
-      .url(forResource: self.rawValue, withExtension: "txt", subdirectory: "dictionaries")
+      .url(forResource: self.resource, withExtension: "txt", subdirectory: "dictionaries")
   }
+  /// Returns the file content as string if it loads successfully
   public func load() -> String? {
     guard let url = self.url else {return nil}
     return try? String(contentsOf: url)
