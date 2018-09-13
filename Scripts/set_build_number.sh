0f6c704b5c2974e06d6aca1f89510eb196a224ee
Tamara Cook <tamara.cook@icloud.com>
2018-09-13 02:47:51 +0200

    Build numbers are based on the number of commits from the current version tag

--- Scripts/set_build_number.sh
+++ Scripts/set_build_number.sh
@@ -1,15 +1,19 @@
-#!/bin/bash
+# ---------------------------- IMPORTANT ----------------------------
+# You must set GITHash to something like 'Set by build script' in the file
+# file '<Project Name>-Info.plist' in the 'Supporting Files' group
+# -------------------------------------------------------------------
+#
+# Get the version number from the tag in git and the number of commits as the build number
+#
+appVersion=$(git describe --tags --always --abbrev=0)
+appBuild=$(git describe --long | cut -f 2 -d "-") 
+gitHash=$(git describe --long | cut -f 3 -d "-")
+echo "From GIT Version = $appVersion Build = $appBuild"
 
-git=$(sh /etc/profile; which git)
-number_of_commits=$("$git" rev-list HEAD --count)
-git_release_version=$("$git" describe --tags --always --abbrev=0)
-
-target_plist="$TARGET_BUILD_DIR/$INFOPLIST_PATH"
-dsym_plist="$DWARF_DSYM_FOLDER_PATH/$DWARF_DSYM_FILE_NAME/Contents/Info.plist"
-
-for plist in "$target_plist" "$dsym_plist"; do
-  if [ -f "$plist" ]; then
-    /usr/libexec/PlistBuddy -c "Set :CFBundleVersion $number_of_commits" "$plist"
-    /usr/libexec/PlistBuddy -c "Set :CFBundleShortVersionString ${git_release_version#*v}" "$plist"
-  fi
-done
+#
+# Set the version info in plist file
+#
+/usr/libexec/PlistBuddy -c "Set :CFBundleShortVersionString $appVersion" "${TARGET_BUILD_DIR}/${INFOPLIST_PATH}"
+/usr/libexec/PlistBuddy -c "Set :CFBundleVersion $appBuild" "${TARGET_BUILD_DIR}/${INFOPLIST_PATH}"
+/usr/libexec/PlistBuddy -c "Set :GITHash $gitHash" "${TARGET_BUILD_DIR}/${INFOPLIST_PATH}"
+echo "Updated ${TARGET_BUILD_DIR}/${INFOPLIST_PATH}"
