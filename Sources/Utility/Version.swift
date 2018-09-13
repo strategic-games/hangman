e1da9c21e76828f4c1946e3d3ca922cdb0bd77a9
Tamara Cook <tamara.cook@icloud.com>
2018-09-13 07:55:46 +0200

    Add documentation

--- Sources/Utility/Version.swift
+++ Sources/Utility/Version.swift
@@ -1,11 +1,14 @@
 import Foundation
 
+/// The git-based versioning constants from Info.plist
 public struct Version {
+  /// The name of the last tag, the count of commits from that tag, and the current commit hash
   public static let (short, build, hash) = getVersion()
+  /// A convenience description string
   public static var description: String {
     return "\(Version.short), build \(Version.build), commit hash \(Version.hash)"
   }
-  static func getVersion() -> (String, String, String) {
+  private static func getVersion() -> (String, String, String) {
     let dummy = ("0.0.0", "0", "abc")
     guard let infoPlist = Bundle(identifier: "Utility")?.infoDictionary else {return dummy}
     guard let short = infoPlist["CFBundleShortVersionString"] as? String else {return dummy}
