b2ac32a05d911cb15a926b9cd2381c51fd11126c
Tamara Cook <tamara.cook@icloud.com>
2018-09-19 11:42:40 +0200

    Add xcode project to source control
    * SPM does not support build phases, I need script and resources
    * xcodegen does not interoperate well with spm's dependencies, only carthage
    * Guaka, one of my dependencies does not yet support carthage

--- Package.swift
+++ Package.swift
@@ -7,25 +7,24 @@ let package = Package(
     name: "hangman",
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
-  .executable(name: "research", targets: ["Research"]),
-    .executable(name: "play", targets: ["Play"]),
-    .library(name: "BoardGames", targets: ["Games"])
+  .executable(name: "hangman", targets: ["HangmanCLI"]),
+    .library(name: "HangmanCore", targets: ["Games"])
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-      .package(url: "https://github.com/jakeheis/SwiftCLI", from: "5.0.0"),
+      .package(url: "https://github.com/nsomar/Guaka.git", from: "0.2.0"),
       .package(url: "https://github.com/jpsim/Yams.git", from: "1.0.1")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
       .target(
-        name: "Play",
-        dependencies: ["Games", "SwiftCLI"]),
+        name: "HangmanCLI",
+        dependencies: ["Games", "SimulationDescription", "Guaka", "Yams"]),
       .target(
-        name: "Research",
-        dependencies: ["Games", "SwiftCLI", "Yams"]),
+        name: "SimulationDescription",
+        dependencies: ["Games"]),
         .target(
             name: "Games",
             dependencies: ["Utility"]),
@@ -33,10 +32,14 @@ let package = Package(
           name: "Utility",
           dependencies: []),
         .testTarget(
+          name: "SimulationDescriptionTests",
+          dependencies: ["SimulationDescription"]),
+        .testTarget(
           name: "GamesTests",
           dependencies: ["Games"]),
         .testTarget(
             name: "UtilityTests",
             dependencies: ["Utility"])
-    ]
+    ],
+    swiftLanguageVersions: [.v4, .v4_2]
 )
