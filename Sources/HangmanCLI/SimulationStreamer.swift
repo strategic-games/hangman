75a0d5d7be9ea43b46f54561749a214840d50b46
Tamara Cook <10754072+tamaracha@users.noreply.github.com>
2018-10-20 21:14:33 +0200

    Add utilities to append binary data to a file

---
+++ Sources/HangmanCLI/SimulationStreamer.swift
@@ -0,0 +1,31 @@
+import Foundation
+
+struct SimulationStreamer {
+  let fileName: String
+  let url: URL
+  let fileHandle: FileHandle
+  init?(fileName: String) {
+    self.fileName = fileName
+    url = URL(fileURLWithPath: fileName)
+    if !FileManager.default.fileExists(atPath: url.path) {
+      FileManager.default.createFile(atPath: url.path, contents: nil, attributes: nil)
+    }
+    guard let fileHandle = try? FileHandle(forWritingTo: url) else {return nil}
+    self.fileHandle = fileHandle
+  }
+  func append(_ data: Data) {
+    fileHandle.seekToEndOfFile()
+    fileHandle.write(data)
+  }
+}
+
+extension Data {
+  func append(to url: URL) throws {
+    if let fileHandle = try? FileHandle(forWritingTo: url) {
+      fileHandle.seekToEndOfFile()
+      fileHandle.write(self)
+    } else {
+      try write(to: url)
+    }
+  }
+}
