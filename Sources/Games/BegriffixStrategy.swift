ba55fcd4db29e964bbc0f98625e804ee5d55a2d2
Tamara Cook <10754072+tamaracha@users.noreply.github.com>
2018-11-14 13:19:11 +0100

    Refactor player strategy with strategy design pattern

---
+++ Sources/Games/BegriffixStrategy.swift
@@ -0,0 +1,60 @@
+/// Strategies for move selection
+public typealias BegriffixHits = [Place: [Begriffix.Word]]
+public typealias BegriffixStrategy = (BegriffixHits, Begriffix) -> Begriffix.Move?
+
+/// Select a random move if possible
+public func randomBegriffixStrategy(hits: BegriffixHits, game: Begriffix) -> Begriffix.Move? {
+  guard let (place, words) = hits.randomElement() else {return nil}
+  guard let word = words.randomElement() else {return nil}
+  return .init(place, word, hits)
+}
+
+/// Select a random word from the shortest pattern
+public func shortBegriffixStrategy(hits: BegriffixHits, game: Begriffix) -> Begriffix.Move? {
+  guard let (place, words) = hits.min(by: {
+    let left = game.pattern(of: $0.key)
+    let right = game.pattern(of: $1.key)
+    return left.count <= right.count
+  }) else {return nil}
+guard let word = words.randomElement() else {return nil}
+return .init(place, word, hits)
+}
+
+/// Select a random word from the longest pattern
+public func longBegriffixStrategy(hits: BegriffixHits, game: Begriffix) -> Begriffix.Move? {
+  guard let (place, words) = hits.max(by: {
+    let left = game.pattern(of: $0.key)
+    let right = game.pattern(of: $1.key)
+    return left.count <= right.count
+  }) else {return nil}
+  guard let word = words.randomElement() else {return nil}
+  return .init(place, word, hits)
+}
+
+/// Prefer words for patterns starting with the given letters
+public func availabilityBegriffixStrategy(hits: BegriffixHits, game: Begriffix) -> Begriffix.Move? {
+  guard let (place, words) = hits.min(by: {
+    let left = game.pattern(of: $0.key).firstIndex(where: {$0 != nil}) ?? 0
+    let right = game.pattern(of: $1.key).firstIndex(where: {$0 != nil}) ?? 0
+    return left <= right
+  }) else {return nil}
+  guard let word = words.randomElement() else {return nil}
+  return .init(place, word, hits)
+}
+
+/// Minimize the number of places for the next player
+public func minPlacesBegriffixStrategy(hits: BegriffixHits, game: Begriffix) -> Begriffix.Move? {
+  do {
+    guard let (place, words) = try hits.min(by: {
+      var leftGame = game
+      var rightGame = game
+      try leftGame.insert(.init($0.key, $0.value[0]))
+      try rightGame.insert(.init($1.key, $1.value[0]))
+      return leftGame.find()?.count ?? 0 <= rightGame.find()?.count ?? 0
+    }) else {return nil}
+    guard let word = words.randomElement() else {return nil}
+    return .init(place, word, hits)
+  } catch {
+    return nil
+  }
+}
--- Sources/Games/Player.swift
+++ Sources/Games/Player.swift
@@ -2,57 +2,18 @@ import Utility
 
 /// A Begriffix player that selects by random
 public struct Player {
-  /// Preferences for move selection
-  public enum Preference: Int {
-    /// Select a random move if possible
-    case random = 0
-    /// Prefer words for patterns starting with the given letters
-    case availability
-    /// Select the longest word
-    case long
-    /// Select the shortest word
-    case short
-    func select(_ hits: [Place: [Begriffix.Word]], game: Begriffix) -> Begriffix.Move? {
-      let hit: (key: Place, value: [Begriffix.Word])?
-      switch self {
-      case .random:
-        hit = hits.randomElement()
-      case .availability:
-        hit = hits.min(by: {
-          let left = game.pattern(of: $0.key).firstIndex(where: {$0 != nil}) ?? 0
-          let right = game.pattern(of: $1.key).firstIndex(where: {$0 != nil}) ?? 0
-          return left <= right
-        })
-      case .long:
-        hit = hits.max(by: {
-          let left = game.pattern(of: $0.key)
-          let right = game.pattern(of: $1.key)
-          return left.count <= right.count
-        })
-      case .short:
-        hit = hits.min(by: {
-          let left = game.pattern(of: $0.key)
-          let right = game.pattern(of: $1.key)
-          return left.count <= right.count
-        })
-      }
-      guard let (place, words) = hit else {return nil}
-      guard let word = words.randomElement() else {return nil}
-      return .init(place, word, hits)
-    }
-  }
   private let vocabulary: Radix
-  let preference: Preference
+  let begriffixStrategy: BegriffixStrategy
   /// Initialize a new player
-  public init(_ vocabulary: Radix, preference: Preference? = nil) {
+  public init(_ vocabulary: Radix, begriffixStrategy: BegriffixStrategy? = nil) {
     self.vocabulary = vocabulary
-    self.preference = preference ?? .random
+    self.begriffixStrategy = begriffixStrategy ?? randomBegriffixStrategy
   }
-  public init<S: Sequence>(_ vocabulary: S, preference: Preference? = nil) where S.Element == String {
+  public init<S: Sequence>(_ vocabulary: S, begriffixStrategy: BegriffixStrategy? = nil) where S.Element == String {
     let radix = Radix()
     radix.insert(vocabulary)
     self.vocabulary = radix
-    self.preference = preference ?? .random
+    self.begriffixStrategy = begriffixStrategy ?? randomBegriffixStrategy
   }
   public func move(_ game: Begriffix) -> Begriffix.Move? {
     guard let places = game.find() else {return nil}
@@ -61,7 +22,7 @@ public struct Player {
       let matches = match(game, place: $0)
       if !matches.isEmpty {hits[$0] = matches}
     }
-    return preference.select(hits, game: game)
+    return begriffixStrategy(hits, game)
   }
   /// Find the words that could be inserted at the given place
   func match(_ game: Begriffix, place: Place) -> [Begriffix.Word] {
