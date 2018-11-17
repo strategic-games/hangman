7d7de0f751ae14f5fc5dbc3b03fa3f8f818becad
Tamara Cook <10754072+tamaracha@users.noreply.github.com>
2018-11-17 18:04:07 +0100

    Fix issues with overflow arithmetic in SplitMix64

--- Sources/Utility/CwlRandom.swift
+++ Sources/Utility/CwlRandom.swift
@@ -92,10 +92,10 @@ public struct SplitMix64: RandomNumberGenerator {
     state = seed
   }
   public mutating func next() -> UInt64 {
-    state += 0x9e3779b97f4a7c15
+    state &+= 0x9e3779b97f4a7c15
     var result = state
-    result = (result ^ (result >> 30)) * 0xbf58476d1ce4e5b9
-    result = (result ^ (result >> 27)) * 0x94d049bb133111eb
+    result = (result ^ (result >> 30)) &* 0xbf58476d1ce4e5b9
+    result = (result ^ (result >> 27)) &* 0x94d049bb133111eb
     return result ^ (result >> 31)
   }
 }
