0faa1169ad8a7d6fee6df2776a1a4fb669477b4f
Tamara Cook <10754072+tamaracha@users.noreply.github.com>
2018-12-12 19:44:30 +0100

    Add extension to split Array into chunks of given size

--- Sources/Utility/Matrix.swift
+++ Sources/Utility/Matrix.swift
@@ -234,3 +234,11 @@ extension Range: Codable where Bound: Codable {
     self.init(uncheckedBounds: (lower: lower, upper: upper))
   }
 }
+
+extension Array {
+  public func chunked(into size: Int) -> [[Element]] {
+    return stride(from: 0, to: count, by: size).map {
+      Array(self[$0 ..< Swift.min($0 + size, count)])
+    }
+  }
+}
