fe680eb809b4d3e6d6b954bc0e33a023f4da1228
Tamara Cook <10754072+tamaracha@users.noreply.github.com>
2018-12-13 17:50:19 +0100

    Add more validation to matrix

--- Sources/Utility/Matrix.swift
+++ Sources/Utility/Matrix.swift
@@ -1,50 +1,3 @@
-/// A 2D index
-public struct Point: Hashable {
-  /// The coordinates, starting from zero
-  public let row: Int, column: Int
-  /// Initialize a new point
-  public init(row: Int, column: Int) {
-    assert(row >= 0 && column >= 0, "negative values are not allowed")
-    self.row = row
-    self.column = column
-  }
-}
-
-extension Point: LosslessStringConvertible {
-  /// A textual description of a point in "row;column" format
-  public var description: String {
-    return "\(row);\(column)"
-  }
-  /// Initialize a point from a stirng which is formatted as "row;column"
-  public init?(_ description: String) {
-    let values = description.split(separator: ";").compactMap {Int($0)}
-    guard values.count == 2 else {return nil}
-    self.init(row: values[0], column: values[1])
-  }
-}
-
-/// A 2D index range
-public struct Area: Hashable {
-  /// The row and column ranges
-  public let rows: Range<Int>, columns: Range<Int>
-  public var indices: (Range<Int>, Range<Int>) {return (rows, columns)}
-  /// How many points this would cover
-  public var count: Int {return rows.count*columns.count}
-  /// The upper left point in this area
-  public var start: Point {return Point(row: rows.lowerBound, column: columns.lowerBound)}
-  /// The lower right point in this area
-  public var end: Point {return Point(row: rows.upperBound-1, column: columns.upperBound-1)}
-  /// Initialize a new area
-  public init(rows: Range<Int>, columns: Range<Int>) {
-    self.rows = rows
-    self.columns = columns
-  }
-  /// Indicates if an area contains the given point
-  public func contains(point: Point) -> Bool {
-    return rows.contains(point.row) && columns.contains(point.column)
-  }
-}
-
 /// A 2D grid for game boards etc.
 public struct Matrix<Element> {
   // MARK: Properties
@@ -53,7 +6,11 @@ public struct Matrix<Element> {
   /// A textual representation
   public var description: String {return String(describing: values)}
   /// The total number of elements
-  public var count: Int {return rows*columns}
+  public var count: Int {return rows * columns}
+  /// Indicates if the matrix has equal row and column count
+  public var isSquare: Bool {return rows == columns}
+  /// Indicates if the matrix has even row and column count
+  public var isEven: Bool {return rows % 2 == 0 && columns % 2 == 0}
   /// The elements as rowwise array
   public private(set) var values: [Element]
   // MARK: Initializers
@@ -91,11 +48,11 @@ public struct Matrix<Element> {
   /// Access the element at the given position
   public subscript(row: Int, column: Int) -> Element {
     get {
-      assert(isValid(row: row, column: column))
+      precondition(isValid(row: row, column: column))
       return values[index(row: row, column: column)]
     }
     set {
-      assert(isValid(row: row, column: column))
+      precondition(isValid(row: row, column: column))
       values[index(row: row, column: column)] = newValue
     }
   }
@@ -110,8 +67,8 @@ public struct Matrix<Element> {
       return Matrix(values: Array(self[rows, columns].joined()), rows: rows.count, columns: columns.count)
     }
     set {
-      assert(isValid(rows: rows, columns: columns))
-      assert(rows.count == newValue.rows && columns.count == newValue.columns)
+      precondition(isValid(rows: rows, columns: columns))
+      precondition(rows.count == newValue.rows && columns.count == newValue.columns)
       indices(rows: rows, columns: columns).enumerated().forEach {
         self.values[$0.1] = newValue.row($0.0)
       }
@@ -120,12 +77,12 @@ public struct Matrix<Element> {
   /// Access the elements in the given ranges
   public subscript(rows: Range<Int>, columns: Range<Int>) -> [ArraySlice<Element>] {
     get {
-      assert(isValid(rows: rows, columns: columns), "ranges out of bound")
+      precondition(isValid(rows: rows, columns: columns), "ranges out of bound")
       return indices(rows: rows, columns: columns).map {values[$0]}
     }
     set {
-      assert(isValid(rows: rows, columns: columns))
-      assert(rows.count == newValue.count && columns.count == newValue[0].count)
+      precondition(isValid(rows: rows, columns: columns))
+      precondition(rows.count == newValue.count && columns.count == newValue[0].count)
       indices(rows: rows, columns: columns).enumerated().forEach {
         self.values[$0.1] = newValue[$0.0]
       }
@@ -147,14 +104,14 @@ public struct Matrix<Element> {
   }
   /// Returns the elements in the given row
   public func row(_ index: Int) -> ArraySlice<Element> {
-  assert(index >= 0 && index < rows)
+  precondition((0..<rows).contains(index))
     let start = columns*index
   let end = columns*(index+1)
   return values[start..<end]
   }
   /// Returns the elements in the given column
   public func column(_ index: Int) -> [Element] {
-    assert(index >= 0 && index < columns)
+    precondition((0..<columns).contains(index))
     return stride(from: index, to: values.count, by: columns).map {values[$0]}
   }
   /// Get the elements row by row
@@ -187,13 +144,23 @@ public struct Matrix<Element> {
     return rows.map {index(row: $0, column: columns.lowerBound)..<index(row: $0, column: columns.upperBound)}
   }
   /// Indicate if a matrix contains the given coordinates
-  func isValid(row: Int, column: Int) -> Bool {
+  public func isValid(row: Int, column: Int) -> Bool {
     return (0..<rows).contains(row) && (0..<columns).contains(column)
   }
+  public func isValid(point: Point) -> Bool {
+    return isValid(row: point.row, column: point.column)
+  }
   /// Indicate if a matrix contains the given 2D range
-  func isValid(rows: Range<Int>, columns: Range<Int>) -> Bool {
+  public func isValid(rows: Range<Int>, columns: Range<Int>) -> Bool {
     return rows.lowerBound >= 0 && rows.upperBound <= self.rows &&
       columns.lowerBound >= 0 && columns.upperBound <= self.columns
+  }
+  public func isValid(area: Area) -> Bool {
+    return isValid(rows: area.rows, columns: area.columns)
+  }
+  /// Returns a matrix containing the results of mapping the given closure over the matrix’s elements.
+  public func map<T>(_ transform: (Element) -> T) -> Matrix<T> {
+    return Matrix<T>(values: values.map(transform), rows: rows, columns: columns)
   }
 }
 
@@ -205,40 +172,5 @@ extension Matrix: CustomStringConvertible where Element == Character {
       str.append("\n")
     }
     return str
-  }
-}
-extension Point: Codable {}
-
-extension Range: Codable where Bound: Codable {
-  enum CodingKeys: CodingKey {
-    case lower, upper
-  }
-  public func encode(to encoder: Encoder) throws {
-    let dict = [CodingKeys.lower.stringValue: lowerBound, CodingKeys.upper.stringValue: upperBound]
-    try dict.encode(to: encoder)
-  }
-  public init(from decoder: Decoder) throws {
-    let dict = try [String: Bound](from: decoder)
-    guard let lower = dict[CodingKeys.lower.stringValue] else {
-      throw DecodingError.valueNotFound(
-        Bound.self,
-        .init(codingPath: decoder.codingPath + [CodingKeys.lower], debugDescription: "lowerBound not found")
-      )
-    }
-    guard let upper = dict[CodingKeys.upper.stringValue] else {
-      throw DecodingError.valueNotFound(
-        Bound.self,
-        .init(codingPath: decoder.codingPath + [CodingKeys.upper], debugDescription: "upperBound not found")
-      )
-    }
-    self.init(uncheckedBounds: (lower: lower, upper: upper))
-  }
-}
-
-extension Array {
-  public func chunked(into size: Int) -> [[Element]] {
-    return stride(from: 0, to: count, by: size).map {
-      Array(self[$0 ..< Swift.min($0 + size, count)])
-    }
   }
 }
